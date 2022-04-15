dbd760eceef2bf77e9b2d3db40b8a1c4909a3cb3
Kevin Nar <45304201+kpnaranj@users.noreply.github.com>
2022-04-15 13:18:35 -0400

    feat(Basket items): Creating a basket item to successfully store items in database. #TAD-011
    The following code updates the basket elements endpoints to be able to receive data elements and remove them successfully.

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,35 +18,80 @@ namespace API.Controllers
 
         }
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket()
+        public async Task<ActionResult<BasketDto>> GetBasket()
         {
-            var basket = await _context.Baskets
-            .Include(i => i.Items)
-            .ThenInclude(p => p.Product)
-            .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+            var basket = await RetrieveBasket();
 
             if (basket == null) return NotFound();
 
-            return basket;
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Brand = item.Product.Brand,
+                    Type = item.Product.Type,
+                    Quantity = item.Quantity
+                }).ToList()
+
+            };
         }
+
         [HttpPost]
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
-        {   // 1. Get Basket
-            // 2. If basket does not exists Create it
-            // 2. Get Product 
-            // 3. Add Item
-            // 4. Save Changes
+        {
+            var basket = await RetrieveBasket();
+            if (basket == null) basket = CreateBasket();
 
-            return StatusCode(201);
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) return NotFound();
+
+            basket.AddItem(product, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return StatusCode(201);
+
+            return BadRequest(new ProblemDetails { Title = "Problem saving item to basket" });
         }
 
         [HttpDelete]
         public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
         {
-            // 1. Get basket
-            // 2. Remove item or reduce the basket
-            // 3. Save changes
-            return Ok();
+            var basket = await RetrieveBasket();
+            if (basket == null) return NotFound();
+
+            basket.RemoveItem(productId, quantity);
+
+            var result = await _context.SaveChangesAsync() > 0;
+            if (result) return Ok();
+
+            return BadRequest(new ProblemDetails { Title = "Problems removing item to basket" });
+        }
+
+        private async Task<Basket> RetrieveBasket()
+        {
+            return await _context.Baskets
+                        .Include(i => i.Items)
+                        .ThenInclude(p => p.Product)
+                        .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+        }
+
+        private Basket CreateBasket()
+        {
+            var buyerId = Guid.NewGuid().ToString();
+            var cookieOptions = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(30) };
+            Response.Cookies.Append("buyerId", buyerId, cookieOptions);
+            var basket = new Basket { BuyerId = buyerId };
+
+            _context.Baskets.Add(basket);
+
+            return basket;
         }
     }
 }
